7e67a1c7d7c2ccc419bab12891640af65973aa30
David R. Hanson <drh@drhanson.net>
2021-01-11 10:03:09 -0700

    Reset to an empty working directory.

--- include/atom.h
+++
@@ -1,8 +0,0 @@
-/* $Id$ */
-#ifndef ATOM_INCLUDED
-#define ATOM_INCLUDED
-extern       int   Atom_length(const char *str);
-extern const char *Atom_new   (const char *str, int len);
-extern const char *Atom_string(const char *str);
-extern const char *Atom_int   (long n);
-#endif
--- src/assert.c
+++
@@ -1,6 +0,0 @@
-static char rcsid[] = "$Id$";
-#include "assert.h"
-const Except_T Assert_Failed = { "Assertion failed" };
-void (assert)(int e) {
-	assert(e);
-}
